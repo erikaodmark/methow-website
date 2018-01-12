c0c5856a3bae97a96833cebd51cf00ea44f91dc6
Erika Odmark <erikaodmark@Erikas-MacBook-Air-10.local>
2018-01-12 10:15:50 -0800

    updates

--- EduComp/sendemail.php
+++ EduComp/sendemail.php
@@ -5,7 +5,7 @@ ini_set("error_log", "errors/errors.log");
 */
 
 $email_id = "erika.odmark@gmail.com";                     // Replace with your email address
-	
+echo $_POST;
 if(isset($_POST["submit"]))
 {
 	$contact_name = $_POST['name'];
@@ -34,45 +34,45 @@ if(isset($_POST["submit"]))
     else if(isset($contact_message) && strlen(trim($contact_message)) < 1) {
 		$result = 'Error : Please enter your message.';
     }
-	
+
 	else
 	{
 		$to = $contact_email;
- 
+
 		$headers = "From: $email_id" . "\r\n" . "MIME-Version: 1.0" . "\r\n" . "Content-type: text/html; charset=iso-8859-1";
-		
+
 		$subject = "We have received your message, we will get back to you shortly.";
 
 		$email_msg = "<div><p>Hello,</p><p>This is an automated response email.</p><p>We have received the following query from you - </p><p>Name : $contact_name</p><p>Email : $contact_email</p><p>Subject : $contact_subject</p><p>Message : $contact_message</p><p>Thank you for contacting us.</p><p>We will get back to you shortly.</p><p>Best Regards</p></div>";
-		
+
 		$mail_sent = mail($to,$subject,$email_msg,$headers);
 
 		if($mail_sent == false)
 			$result = "Error : Email could not be sent, please try again later.";
 
 		else if($mail_sent == true)
-		{	
+		{
 			$to = $email_id;
-			
+
 			$headers = "From: $email_id" . "\r\n" . "MIME-Version: 1.0" . "\r\n" . "Content-type: text/html; charset=iso-8859-1";
-		
+
 			$subject = "You have a new query from your website";
 
 			$email_msg = "<div><p>Hello,</p><p>You have received a new query from your website, following are the details : </p><p>Name : $contact_name</p><p>Email : $contact_email</p><p>Subject : $contact_subject</p><p>Message : $contact_message</p></div>";
-			
+
 			$mail_sent_2 = mail($to,$subject,$email_msg,$headers);
-	
+
 			if($mail_sent_2 == false)
 				$result = "Error : Email could not be sent, please try again later.";
-			
+
 			else
 				$result = "Success : We have received your query, thank you.";
-		}		
+		}
 	}
 }
 else
-{	
-	header("Location:index.html");
-	exit;	
-}	
+{
+	// header("Location:index.html");
+	// exit;
+}
 ?>
